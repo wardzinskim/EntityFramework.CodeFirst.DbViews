f374ad5a96dfb98d47527b3121dcbc69c8d623c0
Marcin Wardzinski <wardzinskim@outlook.com>
2018-01-03 21:34:20 +0100

    update assembly info

--- src/EntityFramework.CodeFirstDbViews/Properties/AssemblyInfo.cs
+++ src/EntityFramework.CodeFirstDbViews/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EntityFramework.CodeFirstDbViews")]
+[assembly: AssemblyTitle("EntityFramework.CodeFirst.DbViews")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EntityFramework.CodeFirstDbViews")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("EntityFramework.CodeFirst.DbViews")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
